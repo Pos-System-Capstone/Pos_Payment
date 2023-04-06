d00b265ab1bde0240257e6f0629172a2b605cb10
dat-dang <dat.dang@billidentity.com>
2023-04-06 22:23:48 +0700

    Set-up service

---
+++ ResoPayment/Service/BaseService.cs
@@ -0,0 +1,16 @@
+﻿using ResoPayment.ApplicationCore.Interfaces;
+using ResoPayment.Infrastructure.Models;
+
+namespace ResoPayment.Service;
+
+public abstract class BaseService<T> where T : class
+{
+	protected IUnitOfWork<PosPaymentContext> _unitOfWork;
+	protected ILogger<T> _logger;
+
+	public BaseService(IUnitOfWork<PosPaymentContext> unitOfWork, ILogger<T> logger)
+	{
+		_unitOfWork = unitOfWork;
+		_logger = logger;
+	}
+}
---
+++ ResoPayment/Service/Implements/TransactionService.cs
@@ -0,0 +1,11 @@
+﻿using ResoPayment.ApplicationCore.Interfaces;
+using ResoPayment.Infrastructure.Models;
+
+namespace ResoPayment.Service.Implements;
+
+public class TransactionService : BaseService<TransactionService>
+{
+	public TransactionService(IUnitOfWork<PosPaymentContext> unitOfWork, ILogger<TransactionService> logger) : base(unitOfWork, logger)
+	{
+	}
+}
