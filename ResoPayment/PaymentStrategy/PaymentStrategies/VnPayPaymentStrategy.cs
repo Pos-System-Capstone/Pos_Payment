797be3c6f82c1853e4550b73e05b39ce7cbf9c10
Endy <nguyenduong971999@gmail.com>
2023-04-22 16:02:02 +0700

    update bankCode request data for VnPay

--- ResoPayment/PaymentStrategy/PaymentStrategies/VnPayPaymentStrategy.cs
+++ ResoPayment/PaymentStrategy/PaymentStrategies/VnPayPaymentStrategy.cs
@@ -43,6 +43,7 @@ public class VnPayPaymentStrategy : IPaymentStrategy
         pay.AddRequestData("vnp_CurrCode", _vnPayConfig.CurrCode);
         pay.AddRequestData("vnp_IpAddr", pay.GetIpAddress(_context));
         pay.AddRequestData("vnp_Locale", "vn");
+        pay.AddRequestData("vnp_BankCode", "VNPAYQR");
         pay.AddRequestData("vnp_OrderInfo", _orderDescription);
         pay.AddRequestData("vnp_ReturnUrl", urlCallBack);
         pay.AddRequestData("vnp_TxnRef", _orderId.ToString());
