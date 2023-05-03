496409efa2e66c1b0d27d4ed3a855a8efa89be0c
dat-dang <dat.dang@billidentity.com>
2023-05-04 00:48:56 +0700

    Add Add new and update

--- ResoPayment/Service/Implements/BrandService.cs
+++ ResoPayment/Service/Implements/BrandService.cs
@@ -128,8 +128,132 @@ public class BrandService : BaseService<BrandService>, IBrandService
 			await _unitOfWork.GetRepository<Brand>().InsertAsync(newBrand);
 			return await _unitOfWork.CommitAsync() > 0;
 		}
+		else
+		{
+			IEnumerable<BrandPaymentProviderMapping> brandPaymentProviderMappings = await _unitOfWork
+				.GetRepository<BrandPaymentProviderMapping>().GetListAsync(predicate: x => x.BrandId.Equals(brand.Id), include: x => x.Include(x => x.PaymentProvider));
+			if(!brandPaymentProviderMappings.Any(x => x.PaymentProvider.Type.ToUpper().Equals("VIETQR")) && request.VietQrConfigRequest != null)
+			{
+				PaymentProvider vietQrPaymentProvider = await _unitOfWork.GetRepository<PaymentProvider>()
+					.SingleOrDefaultAsync(predicate: x => x.Type.ToUpper().Equals("VIETQR"));
+				VietQRConfig vietQrConfig = new VietQRConfig()
+				{
+					BankCode = request.VietQrConfigRequest.BankCode,
+					AccountNo = request.VietQrConfigRequest.AccountNumber,
+					AccountName = request.VietQrConfigRequest.AccountName
+				};
+				string vietQrConfigSerialize = JsonConvert.SerializeObject(vietQrConfig);
+				BrandPaymentProviderMapping newVietQrBrandPaymentProviderMapping = new BrandPaymentProviderMapping()
+				{
+					Id = Guid.NewGuid(),
+					BrandId = brand.Id,
+					Status = "Active",
+					PaymentProviderId = vietQrPaymentProvider.Id,
+					Config = vietQrConfigSerialize
+				};
+				await _unitOfWork.GetRepository<BrandPaymentProviderMapping>()
+					.InsertAsync(newVietQrBrandPaymentProviderMapping);
+			}
+			if (!brandPaymentProviderMappings.Any(x => x.PaymentProvider.Type.ToUpper().Equals("ZALOPAY")) && request.ZaloPayConfigRequest != null)
+			{
+				PaymentProvider zaloPayPaymentProvider = await _unitOfWork.GetRepository<PaymentProvider>()
+					.SingleOrDefaultAsync(predicate: x => x.Type.ToUpper().Equals("ZALOPAY"));
+				ZaloPayConfig zaloPayConfig = new ZaloPayConfig()
+				{
+					AppId = request.ZaloPayConfigRequest.AppId,
+					BankCode = "zalopayapp",
+					Key2 = request.ZaloPayConfigRequest.Key2,
+					Key1 = request.ZaloPayConfigRequest.Key1,
+					BaseUrl = "https://sandbox.zalopay.com.vn/v001/tpe/createorder"
+				};
+				string zaloPayConfigSerialize = JsonConvert.SerializeObject(zaloPayConfig);
+				BrandPaymentProviderMapping newZaloPayBrandPaymentProviderMapping = new BrandPaymentProviderMapping()
+				{
+					Id = Guid.NewGuid(),
+					BrandId = brand.Id,
+					Status = "Active",
+					PaymentProviderId = zaloPayPaymentProvider.Id,
+					Config = zaloPayConfigSerialize
+				};
+				await _unitOfWork.GetRepository<BrandPaymentProviderMapping>()
+					.InsertAsync(newZaloPayBrandPaymentProviderMapping);
+			}
 
-		return false;
+			if (!brandPaymentProviderMappings.Any(x => x.PaymentProvider.Type.ToUpper().Equals("VNPAY")) && request.VnPayConfigRequest != null)
+			{
+				PaymentProvider vnPayPaymentProvider = await _unitOfWork.GetRepository<PaymentProvider>()
+					.SingleOrDefaultAsync(predicate: x => x.Type.ToUpper().Equals("VNPAY"));
+				VnPayConfig vnPayConfig = new VnPayConfig()
+				{
+					BaseUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
+					SecureHash = request.VnPayConfigRequest.SecureHash,
+					TmnCode = request.VnPayConfigRequest.TmnCode,
+					Version = "2.1.0",
+					Command = "pay",
+					CurrCode = "VND"
+				};
+				string vnPayConfigSerialize = JsonConvert.SerializeObject(vnPayConfig);
+				BrandPaymentProviderMapping newVnPayBrandPaymentProviderMapping = new BrandPaymentProviderMapping()
+				{
+					Id = Guid.NewGuid(),
+					BrandId = brand.Id,
+					Status = "Active",
+					PaymentProviderId = vnPayPaymentProvider.Id,
+					Config = vnPayConfigSerialize
+				};
+				await _unitOfWork.GetRepository<BrandPaymentProviderMapping>()
+					.InsertAsync(newVnPayBrandPaymentProviderMapping);
+			}
+			
+			foreach (var brandPaymentProviderMapping in brandPaymentProviderMappings)
+			{
+				if (brandPaymentProviderMapping.PaymentProvider.Type.ToUpper().Equals("VIETQR") && request.VietQrConfigRequest != null)
+				{
+					VietQRConfig vietQrConfig = new VietQRConfig()
+					{
+						BankCode = request.VietQrConfigRequest.BankCode,
+						AccountNo = request.VietQrConfigRequest.AccountNumber,
+						AccountName = request.VietQrConfigRequest.AccountName
+					};
+					string vietQrConfigSerialize = JsonConvert.SerializeObject(vietQrConfig);
+					brandPaymentProviderMapping.Config = vietQrConfigSerialize;
+					_unitOfWork.GetRepository<BrandPaymentProviderMapping>().UpdateAsync(brandPaymentProviderMapping);
+				}
+
+				if (brandPaymentProviderMapping.PaymentProvider.Type.ToUpper().Equals("ZALOPAY") && request.ZaloPayConfigRequest != null)
+				{
+					ZaloPayConfig zaloPayConfig = new ZaloPayConfig()
+					{
+						AppId = request.ZaloPayConfigRequest.AppId,
+						BankCode = "zalopayapp",
+						Key2 = request.ZaloPayConfigRequest.Key2,
+						Key1 = request.ZaloPayConfigRequest.Key1,
+						BaseUrl = "https://sandbox.zalopay.com.vn/v001/tpe/createorder"
+					};
+					string zaloPayConfigSerialize = JsonConvert.SerializeObject(zaloPayConfig);
+					brandPaymentProviderMapping.Config = zaloPayConfigSerialize;
+					_unitOfWork.GetRepository<BrandPaymentProviderMapping>().UpdateAsync(brandPaymentProviderMapping);
+				}
+
+				if (brandPaymentProviderMapping.PaymentProvider.Type.ToUpper().Equals("VNPAY") && request.VnPayConfigRequest != null)
+				{
+					VnPayConfig vnPayConfig = new VnPayConfig()
+					{
+						BaseUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
+						SecureHash = request.VnPayConfigRequest.SecureHash,
+						TmnCode = request.VnPayConfigRequest.TmnCode,
+						Version = "2.1.0",
+						Command = "pay",
+						CurrCode = "VND"
+					};
+					string vnPayConfigSerialize = JsonConvert.SerializeObject(vnPayConfig);
+					brandPaymentProviderMapping.Config = vnPayConfigSerialize;
+					_unitOfWork.GetRepository<BrandPaymentProviderMapping>().UpdateAsync(brandPaymentProviderMapping);
+				}
+			}
+
+			return await _unitOfWork.CommitAsync() > 0;
+		}
 	}
 
 	public async Task<GetBrandPaymentProviderMappingResponse> GetBrandPaymentProviderMapping(Guid brandId)
